e9ccb9e19ccc5781f5e8fae4cff719b85ec7def6
zoixs <zoixs@outlook.com>
2025-08-21 00:31:14 +0800

    add feature of tempo change,but librosa output sucks..

--- audio-adjust.py
+++ audio-adjust.py
@@ -1,12 +1,11 @@
 # Beat tracking example
 import librosa
-
-
+import soundfile
 # 1. 文件路径
 filename = r"D:\temp work\Programme\Audio bpm adjust\base files\Requiem of fate.mp3"
-
+savepath = r"D:\temp work\Programme\Audio bpm adjust\base files\Requiem of fate adjust bpm version.wav"
 # 2. 读取音频（保持原采样率）
-y, sr = librosa.load(filename, sr=None)
+y, sr = librosa.load(filename, sr=48000)
 
 # 3. 节拍检测
 tempo = librosa.beat.beat_track(y=y, sr=sr)[0]
@@ -16,7 +15,8 @@ Bpm = round((tempo)[0],3)
 Target = 180
 Tolerance = 50
 
-#如果bpm与180差距小于tolerance，则以原bpm作为处理素材，否则将原bpm*2或/2作为基础数据（即以原曲1/2拍作为一拍），如仍达不到需求则继续循环  
+#4. 原始bpm修改
+#   如果bpm与180差距小于tolerance，则以原bpm作为处理素材，否则将原bpm*2或/2作为基础数据（即以原曲1/2拍作为一拍），如仍达不到需求则继续循环  
 def adjust_bpm(bpm,target,tolerance):
     if abs(bpm - target) > tolerance:
         while bpm > target + tolerance:
@@ -26,6 +26,10 @@ def adjust_bpm(bpm,target,tolerance):
     return bpm
 FinalBpm = adjust_bpm(Bpm,Target,Tolerance)
 
-
-
 print(FinalBpm)
+
+# 5. 修改歌曲速度匹配Target
+StrechedResult = librosa.effects.time_stretch(y=y,rate=(Target/FinalBpm))
+print(type(StrechedResult))
+
+soundfile.write(savepath,StrechedResult,samplerate=48000,subtype='PCM_24')
